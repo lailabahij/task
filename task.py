b4e05b25d4cafdecb6c9be7c443ff760085cd64a
lailabahij <lailabahij13@gmail.com>
2024-11-28 09:49:55 +0100

    first commit

---
+++ task.py
@@ -0,0 +1,9 @@
+print("Programming is like building a multilingual puzzle")
+number=98
+print("///////////////////////////////////////////////////////")
+print(f"{number}Battery street")
+print("///////////////////////////////////////////////////////")
+number = 3.14159
+print(f"Float:{number}")
+str = "laila"
+print((str+'\n')*3)
